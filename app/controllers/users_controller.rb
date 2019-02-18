857c7a5e9cf055a3120bfb3e38f2b97acf60d0da
Connie Hyman <constance.hyman@mchsi.com>
2019-02-18 19:20:55 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
     erb :'/users/create_user'
   end
 
-  
+  post  '/signup' do
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+  end
 end
