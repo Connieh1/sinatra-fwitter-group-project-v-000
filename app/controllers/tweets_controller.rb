7ef0d7cab5df3e10bb623eaa3041ffce8d2f19c8
Connie Hyman <constance.hyman@mchsi.com>
2019-02-18 19:41:49 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,10 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if logged_in?
-    @tweets = Tweet.all
-    erb :'/tweets/tweets'
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else
+      redirect '/login'
+    end
   end
 end
