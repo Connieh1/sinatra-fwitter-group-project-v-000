dc07fe2f1212ffb383b2bd126d56d05a142f8903
Connie Hyman <constance.hyman@mchsi.com>
2019-02-18 19:57:54 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,7 @@ class UsersController < ApplicationController
   post  '/signup' do
     if params[:username] = "" || params[:email] = "" || params[:password] = ""
       redirect '/signup'
-    else
-  #     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-  #     session[:user_id] = @user_id
-      redirect '/tweets'
+  
     end
   end
 end
