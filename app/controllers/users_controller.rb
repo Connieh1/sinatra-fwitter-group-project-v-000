26174fe8997002de499f3f626185608f33913ea5
Connie Hyman <constance.hyman@mchsi.com>
2019-02-18 22:07:00 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,38 +6,47 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-      erb :'/users/create_user'
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
     else
-      redirect '/tweets'
+      redirect to '/tweets'
     end
   end
 
-  post  '/signup' do
+  post '/signup' do
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect '/signup'
+      redirect to '/signup'
     else
-      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
-      session[:user_id] = @user_id
-      redirect '/tweets'
+      session[:user_id] = @user.id
+      redirect to '/tweets'
     end
   end
 
   get '/login' do
     if !logged_in?
-      erb :'/users/login'
+      erb :'users/login'
     else
       redirect '/tweets'
     end
   end
 
   post '/login' do
-    user = User.find_by(username: params[:username])
+    user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user_id
-      redirect '/tweets'
+      session[:user_id] = user.id
+      redirect "/tweets"
     else
-      redirect '/signup'
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
     end
   end
 end
